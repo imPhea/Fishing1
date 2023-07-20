f15d7b0db66cfb7585fb6e3df362d6e3570b1d39
VC_Sophea <111986860+imPhea@users.noreply.github.com>
2023-07-20 11:05:59 +0700

    Add files via upload

---
+++ src/settings.a2daa.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["bullet","fish"],collisionMatrix:[[false,true],[true,false]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/start.fire",orientation:"portrait",jsList:["assets/Script/Lib/Fetch.edf16.js"],bundleVers:{internal:"3c089",resources:"cfd36",main:"b8c9d"}};
